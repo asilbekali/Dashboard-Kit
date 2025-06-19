04fcbad1e36655036e6b092b16eb5ee8012d2f6b
asilbekali <asilbekabdugafforov7@gmail.com>
2025-06-19 12:59:19 +0500

    add user tables

--- src/pages/Tickets.jsx
+++ src/pages/Tickets.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import TableTopAction from "../components/TableTopAction"
+import TicketTable from '../modules/TicketTable'
 
 const Tickets = () => {
   return (
-    <div className='bg-white w-[100%] h-[942px] rounded-[8px] border-[1px] border-[#DFE0EB]'>
+    <div className='bg-white w-[100%]  rounded-[8px] border-[1px] border-[#DFE0EB]'>
       <div className='bg-white px-[32px] mt-[32px] pb-[48px] flex items-center justify-between'>
         <h2 className='font-bold text-[19px]'>All tickets</h2>
         <div className='flex items-center gap-[15px]'>
@@ -11,6 +12,7 @@ const Tickets = () => {
           <button className='border-none p-[7px] rounded-[20px] w-[120px] bg-green-700 text-white hover:shadow-lg shadow-black duration-300'>Add t icket</button>
         </div>
       </div>
+      <TicketTable />
     </div>
   )
 }
